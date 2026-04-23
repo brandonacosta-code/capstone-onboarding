b63d6e1b4b1e9cb0f287426635f943ca2e856df9
Brandon Acosta <bacosta@indiepay.co>
2026-04-23 16:19:22 -0600

    ODV-90: Add product search and sort UI automation test

---
+++ Capstone.UITests/Products/Pages/ProductSearchPage.cs
@@ -0,0 +1,23 @@
+﻿using Boa.Constrictor.Playwright;
+using Microsoft.Playwright;
+
+namespace Capstone.UITests.Products.Pages;
+
+public static class ProductSearchPage
+{
+	public static IPlaywrightLocator SearchInput =>
+		PlaywrightLocator.L("Search Input",
+			page => page.Locator("#searchInput"));
+
+	public static IPlaywrightLocator SortBySelect =>
+		PlaywrightLocator.L("Sort By Select",
+			page => page.Locator("#sortBySelect"));
+
+	public static IPlaywrightLocator SortDirectionSelect =>
+		PlaywrightLocator.L("Sort Direction Select",
+			page => page.Locator("#sortDirectionSelect"));
+
+	public static IPlaywrightLocator FirstProductName =>
+		PlaywrightLocator.L("First Product Name",
+			page => page.Locator(".k-table-row:first-child .k-table-td:first-child"));
+}
---
+++ Capstone.UITests/Products/Tests/ProductSearchTest.cs
@@ -0,0 +1,27 @@
+﻿using Allure.NUnit.Attributes;
+using Boa.Constrictor.Playwright;
+using Capstone.UITests.Core;
+using Capstone.UITests.Products.Actions;
+using Capstone.UITests.Products.Pages;
+using NUnit.Framework;
+
+namespace Capstone.UITests.Products.Tests;
+
+[TestFixture]
+[AllureSuite("Products")]
+[BoundedContext("Products")]
+public class ProductSearchTests : TestBase
+{
+	[Test]
+	public async Task SearchProducts_SortByName_Ascending_ShowsFilteredResults()
+	{
+		await Actor.AttemptsToAsync(
+			SearchAndSortProducts.With("chocolate", "Name", "Ascending"));
+
+		var firstProduct = await Page
+			.Locator(".k-table-row:first-child .k-table-td:first-child")
+			.InnerTextAsync();
+
+		Assert.That(firstProduct, Does.Contain("Chocolate"));
+	}
+}
